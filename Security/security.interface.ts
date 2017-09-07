f1a65cf5f50770a7bd1aa0edc72693d8e3a1ee3a
JunSung, Kang <kjuns1@naver.com>
2017-09-08 01:06:43 +0900

    Revert "JSKang"
    This reverts commit 87b6ef599b7b6ef6580a98df96e7faa30baa0cca.

--- Security/security.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface SecurityInterface {
-    Base64Encode(data): String;
-    Base64Decode(data): String;
-    Utf8Encode(data): String;
-    Utf8Decode(data): String;
-    Md5Encode(data): String;
-}
-
